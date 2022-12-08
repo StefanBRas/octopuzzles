10a47756619af126f972afa47ec88a12662dae25
sorenholsthansen <soerenhh@pm.me>
2022-12-08 18:09:18 +0100

    add logging to see database_url

--- src/server/trpc/index.ts
+++ src/server/trpc/index.ts
@@ -10,8 +10,10 @@ import votes from './votes';
 import comments from './comments';
 import prisma from '$utils/prisma';
 import { getJwt } from '$utils/jwt/getJwt';
+import type { DATABASE_URL } from '$env/static/private';
 
 export const createContext = async (event: RequestEvent) => {
+  console.log('DATABASE_URL', DATABASE_URL);
   const jwtToken = getJwt(event);
   return { event, prisma, token: jwtToken };
 };
