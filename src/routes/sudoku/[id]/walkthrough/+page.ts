9464adb76be1922648d305d5cf3bde3783e64080
sorenholsthansen <soerenhh@pm.me>
2022-12-10 21:13:28 +0100

    add error to walkthrough page.ts

--- src/routes/sudoku/[id]/walkthrough/+page.ts
+++ src/routes/sudoku/[id]/walkthrough/+page.ts
@@ -1,4 +1,5 @@
 import trpc from '$lib/client/trpc';
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
